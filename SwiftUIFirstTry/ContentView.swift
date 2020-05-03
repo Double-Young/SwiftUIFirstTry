26d805d852037b21855b4fc4305f119d61dae034
DoubleYoung <imdoubleyoung@foxmail.com>
2020-05-03 10:02:50 +0800

    Initial Commit

---
+++ SwiftUIFirstTry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIFirstTry
+//
+//  Created by Young on 2020/5/3.
+//  Copyright © 2020 DoubleYoung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
